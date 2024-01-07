1a4ed03002c9ec27391ef53242a38796e877f368
Ali Kamel <110999470+Alikamel1121@users.noreply.github.com>
2024-01-07 18:04:44 +0330

    021017 - درست کردن برنامه ساز

--- Student assistant software/barname saz.py
+++ Student assistant software/barname saz.py
@@ -4,89 +4,135 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 
 
-def create_week_planner(course_list, namePic):
-    width, height = 1920, 800
-    image = Image.new("RGB", (width, height),(242,243,245))
-    draw = ImageDraw.Draw(image)
-    font_size = 24
-    font = ImageFont.truetype("font\B NAZANIN BOLD_YASDL.COM.TTF", font_size)
+class week_planner:
+    def __init__(self, fontPath, fontSize, width, height, lineColor,textColor, lineWidth, bgColor, outPath, nameImage, days):
+        self.font = ImageFont.truetype(fontPath, fontSize)
+        self.fontSize = fontSize
+        self.width = width
+        self.height = height
+        self.lineColor = lineColor
+        self.textColor = textColor
+        self.lineWidth = lineWidth
+        self.bgColor = bgColor
+        self.outPath = outPath
+        self.nameImage = nameImage
+        self.days = days
+
+    def create_week_planner2(self, course_list):
+        self.image = Image.new("RGB", (self.width, self.height), self.bgColor)
+        draw = ImageDraw.Draw(self.image)
+        height = self.height // len(course_list)
+        height_move = height
+        for course in course_list:
+            text = str(course["Units"]) + " : " + " تعداد واحد ("+course["Course_teacher"]+") " + course["Course_name"]
+            text = course["Course_name"] + " ("+course["Course_teacher"]+") " + " تعداد واحد " + " : " + str(course["Units"]) 
+            draw.text((self.width//2, height_move - (height//2)),
+                      text, fill=self.textColor, font=self.font, anchor="mm",direction="rtl")
+            height_move += height
+        
+        
+    
+    def create_week_planner(self, course_list):
+        self.image = Image.new("RGB", (self.width, self.height), self.bgColor)
+        draw = ImageDraw.Draw(self.image)
+        
+        start_time_main = 8
+        end_time_main = 20
+        num_terms =(end_time_main-start_time_main)//2
+        
+        cell_width = self.width // (num_terms + 1)
+        cell_height = self.height // (len(self.days) + 1)
+        
+        
+        start_time = start_time_main
+        for i in range(num_terms):
+            x = (num_terms - i) * cell_width
+            time_label = f"{start_time + 2:02d}:00 - {start_time:02d}:00"
+            draw.text((x - cell_width // 2, cell_height // 2),
+                      time_label, fill=self.textColor, font=self.font, anchor="mm")
+            draw.line([(x, 0), (x, self.height)],
+                      fill=self.lineColor, width=self.lineWidth)
+            start_time += 2
+            
+        for i, day in enumerate(days):
+            y = (i + 1) * cell_height
+            draw.line([(0, y), (self.width, y)], fill=self.lineColor, width=3)
+            draw.text((self.width - cell_width // 2, y + cell_height // 2),
+                      day, fill=self.textColor, font=self.font, anchor="mm")
+            
+        count_utils = sum(course["Units"] for course in course_list)    
+        draw.text(
+                    (self.width - cell_width // 2, cell_height // 2),
+                    f"تعداد واحد : {count_utils}",
+                    fill=self.textColor,
+                    font=self.font,
+                    anchor="mm",
+                )
+        for course in course_list:
+            course_name = course["Course_name"]
+            if "Time" not in course:
+                continue
+            for item in course["Time"]:
+
+                start_time = datetime.strptime(
+                    item["Start_Time"], "%H:%M").hour
+                end_time = datetime.strptime(
+                    item["End_Time"], "%H:%M").hour
+                course_day = days.index(item["Course_day"])
+                week = item["Week"]
+                y = (course_day + 1) * cell_height
+                x = (num_terms - (start_time - 8)//2) * cell_width
+
+                printloc_x = x - cell_width // 2
+                printloc_y = y + cell_height // 2
+                printText = f"{course_name}"
+                if week == "زوج":
+                    draw.line(
+                        [(x-cell_width, printloc_y), (x, printloc_y)], fill=self.lineColor, width=3)
+                    printloc_y = y + cell_height // 4
+                    printText += f" ({week})"
+                elif week == "فرد":
+                    draw.line(
+                        [(x-cell_width, printloc_y), (x, printloc_y)], fill=self.lineColor, width=3)
+                    printloc_y = y + (cell_height // 4)*3
+                    printText += f" ({week})"
+
+                draw.text(
+                    (printloc_x, printloc_y),
+                    printText,
+                    fill=self.textColor,
+                    font=self.font,
+                    anchor="mm",
+                )
+
+        
+
+
+    def saveImage(self, counter = 0):
+        self.image.save(self.outPath + "/" + self.nameImage + f"{counter}.png")
+
+
+if __name__ == "__main__":
+    with open('out/best_individuals.json', 'r', encoding='utf-8') as f:
+        courses = json.load(f)
+
     days = ["شنبه", "يك شنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنج شنبه"]
-    num_terms = 6
-    cell_width = width // (num_terms + 1)
-    cell_height = height // (len(days) + 1)
-    draw.line([(0, 0), (0, height)], fill="black", width=3)
-    for i in range(num_terms + 1):
-        x = i * cell_width
-        draw.line([(x, 0), (x, height)], fill="black", width=3)
-
-    start_time = 8
-    for i in range(num_terms):
-        x = (num_terms - i) * cell_width
-        time_label = f"{start_time + 2:02d}:00 - {start_time:02d}:00"
-        draw.text((x - cell_width // 2, cell_height // 2),
-                  time_label, fill="black", font=font, anchor="mm")
-        draw.line([(x, cell_height), (x - cell_width, cell_height)],
-                  fill="black", width=2)
-        start_time += 2
-
-    for i, day in enumerate(days):
-        y = (i + 1) * cell_height
-        draw.line([(0, y), (width, y)], fill="black", width=3)
-        draw.text((width - cell_width // 2, y + cell_height // 2),
-                  day, fill="black", font=font, anchor="mm")
-        for j in range(num_terms):
-            for course in course_list:
-                course_name = course["Course_name"]
-                if "Time" not in course:
-                    continue
-                for item in course["Time"]:
-
-                    start_time = datetime.strptime(
-                        item["Start_Time"], "%H:%M").hour
-                    end_time = datetime.strptime(
-                        item["End_Time"], "%H:%M").hour
-                    course_day = days.index(item["Course_day"])
-                    week = item["Week"]
-                    x = (num_terms - j) * cell_width
-                    term_start_time = 8 + 2 * j
-                    term_end_time = term_start_time + 2
-                    if (
-                        i == course_day
-                        and start_time >= term_start_time
-                        and end_time <= term_end_time
-                        and term_start_time >= 8
-                        and term_end_time <= 20
-                    ):
-                        printloc_x = x - cell_width // 2
-                        printloc_y = y + cell_height // 2
-                        printText = f"{course_name}"
-                        if week == "زوج":
-                            draw.line([(x-cell_width, printloc_y), (x,
-                                    printloc_y)], fill="black", width=3)
-                            printloc_y = y + cell_height // 4
-                            printText += f" ({week})"
-                        elif week == "فرد":
-                            draw.line([(x-cell_width, printloc_y), (x,
-                                    printloc_y)], fill="black", width=3)
-                            printloc_y = y + (cell_height // 4)*3
-                            printText += f" ({week})"
-                        
-                        draw.text(
-                            (printloc_x, printloc_y),
-                            printText,
-                            fill="black",
-                            font=font,
-                            anchor="mm",
-                        )
-
-    count_utils = sum(course["Units"] for course in course_list)
-    image.save(namePic+f"_{count_utils}.png")
-
-
-with open('out/best_individuals.json', 'r', encoding='utf-8') as f:
-    courses = json.load(f)
-
-name = "pic/week_planner"
-# create_week_planner(courses,name)
-for i in range(len(courses)):
-    create_week_planner(courses[i], name+f"{i}")
+    font = "font\B NAZANIN BOLD_YASDL.COM.TTF"
+    week_planner_obj = week_planner(
+        font, 24, 1920, 800, "black","black", 3, (242, 243, 245), "./pic", "week_planner", days)
+    
+    week_planner_obj2 = week_planner(
+        font, 24, 1000, 800, "black",(77,88,108), 3, (237, 245, 248), "./pic", "week_ditaile", days)
+    
+    if "Course_name" in courses[0]:
+        week_planner_obj.create_week_planner(courses)
+        week_planner_obj.saveImage()
+        week_planner_obj2.create_week_planner2(courses)
+        week_planner_obj2.saveImage()
+    else:
+        for i in range(len(courses)):
+            week_planner_obj.create_week_planner(courses[i])
+            week_planner_obj.saveImage(i)
+            week_planner_obj2.create_week_planner2(courses[i])
+            week_planner_obj2.saveImage(i)
+# end main
